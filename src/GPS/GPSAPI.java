e2e26a1eed894dbbcdd3c4cfbcccd1afeba0da22
Dibyanshu Kiro <59286017+kiroDK@users.noreply.github.com>
2023-03-31 01:22:35 +0530

    Add files via upload

---
+++ src/GPS/GPSAPI.java
@@ -0,0 +1,42 @@
+package GPS_UPDATED;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GPSAPI {
+
+    private static final Map<String, Double[]> satelliteDatabase = new HashMap<String, Double[]>();
+
+    public static void initDatabase() {
+        satelliteDatabase.put("Andhra Pradesh", new Double[] { 15.9129, 79.7400 });
+        satelliteDatabase.put("Arunachal Pradesh", new Double[] { 28.2180, 94.7278 });
+        satelliteDatabase.put("Assam", new Double[] { 26.2006, 92.9376 });
+        satelliteDatabase.put("Bihar", new Double[] { 25.0961, 85.3131 });
+        satelliteDatabase.put("Chhattisgarh", new Double[] { 21.2787, 81.8661 });
+        satelliteDatabase.put("Goa", new Double[] { 15.2993, 74.1240 });
+        satelliteDatabase.put("Gujarat", new Double[] { 22.2587, 71.1924 });
+        satelliteDatabase.put("Haryana", new Double[] { 29.0588, 76.0856 });
+        satelliteDatabase.put("Himachal Pradesh", new Double[] { 31.1048, 77.1734 });
+        satelliteDatabase.put("Jammu and Kashmir", new Double[] { 33.7782, 76.5762 });
+        satelliteDatabase.put("Jharkhand", new Double[] { 23.6102, 85.2799 });
+        satelliteDatabase.put("Karnataka", new Double[] { 15.3173, 75.7139 });
+        satelliteDatabase.put("Kerala", new Double[] { 10.8505, 76.2711 });
+        satelliteDatabase.put("Madhya Pradesh", new Double[] { 22.9734, 78.6569 });
+        satelliteDatabase.put("Maharashtra", new Double[] { 19.7515, 75.7139 });
+        satelliteDatabase.put("Manipur", new Double[] { 24.6637, 93.9063 });
+        satelliteDatabase.put("Meghalaya", new Double[] { 25.4670, 91.3662 });
+        satelliteDatabase.put("Mizoram", new Double[] { 23.1645, 92.9376 });
+        satelliteDatabase.put("Nagaland", new Double[] { 26.1584, 94.5624 });
+        satelliteDatabase.put("Odisha", new Double[] { 20.9517, 85.0985 });
+        satelliteDatabase.put("Punjab", new Double[] { 31.1471, 75.3412 });
+        satelliteDatabase.put("Rajasthan", new Double[] { 27.0238, 74.2179 });
+        satelliteDatabase.put("Sikkim", new Double[] { 27.5330, 88.5122 });
+        satelliteDatabase.put("Tamil Nadu", new Double[] { 11.1271, 78.6569 });
+        satelliteDatabase.put("Telangana", new Double[] { 18.1124, 79.0192});
+    }
+
+    public static Double[] getLocationCoordinates(String locationName) {
+        return satelliteDatabase.get(locationName);
+    }
+
+}
---
+++ src/GPS/GPSApp.java
@@ -0,0 +1,116 @@
+package GPS_UPDATED;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.text.DecimalFormat;
+import javax.swing.border.Border;
+
+
+public class GPSApp extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+    private JTextField sourceTextField;
+    private JTextField destinationTextField;
+    private JTextField locationTextField;
+
+    public GPSApp() {
+        super("GPS App");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        GPSAPI.initDatabase();
+
+        JPanel inputPanel = new JPanel();
+        Border border = BorderFactory.createEmptyBorder(10, 10, 10, 10);
+
+        JLabel locationLabel = new JLabel("Enter Location Name: ");
+        locationTextField = new JTextField(15);
+
+        JButton getCoordButton = new JButton("Get Location Coordinates");
+        getCoordButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String locationName = locationTextField.getText();
+                Double[] locationCoords = GPSAPI.getLocationCoordinates(locationName);
+                if (locationCoords != null) {
+                    JOptionPane.showMessageDialog(GPSApp.this, "Location Coordinates: " + "("+locationCoords[0] + ", " +locationCoords[1]+")");
+                } else {
+                    JOptionPane.showMessageDialog(GPSApp.this, "Location Not Found", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        JLabel sourceLabel = new JLabel("Enter Source Location Name: ");
+        sourceTextField = new JTextField(15);
+
+        JLabel destinationLabel = new JLabel("Enter Destination Location Name: ");
+        destinationTextField = new JTextField(15);
+
+
+
+        JButton getLocationButton = new JButton("Get Location and Distance");
+        getLocationButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String sourceName = sourceTextField.getText();
+                String destName = destinationTextField.getText();
+                Double[] sourceCoords = GPSAPI.getLocationCoordinates(sourceName);
+                Double[] destCoords = GPSAPI.getLocationCoordinates(destName);
+                if (sourceCoords != null && destCoords != null) {
+                    double distance = calculateDistance(sourceCoords[0], sourceCoords[1], destCoords[0], destCoords[1]);
+                    JOptionPane.showMessageDialog(GPSApp.this, "Source Coordinates: " + "("+ sourceCoords[0] + ", " + sourceCoords[1] +")"
+                            + "\nDestination Coordinates: "+ "(" + destCoords[0] + ", " + destCoords[1] +")"
+                            + "\nDistance between Source and Destination: " + distance + " km");
+                } else {
+                    JOptionPane.showMessageDialog(GPSApp.this, "Location Not Found", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        JLabel creditLabel = new JLabel("Created By:- Dibyanshu Kiro");
+        creditLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        JButton clearButton = new JButton("Clear");
+        clearButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Clear all text fields
+                locationTextField.setText("");
+                sourceTextField.setText("");
+                destinationTextField.setText("");
+            }
+        });
+
+        inputPanel.add(locationLabel);
+        inputPanel.add(locationTextField);
+        inputPanel.add(getCoordButton);
+        inputPanel.add(sourceLabel);
+        inputPanel.add(sourceTextField);
+        inputPanel.add(destinationLabel);
+        inputPanel.add(destinationTextField);
+        inputPanel.add(getLocationButton);
+        inputPanel.add(clearButton);
+        inputPanel.add(creditLabel, BorderLayout.SOUTH);
+        inputPanel.setBorder(BorderFactory.createCompoundBorder(border, BorderFactory.createRaisedBevelBorder()));
+
+        getContentPane().add(inputPanel, BorderLayout.CENTER);
+        setSize(new Dimension(300, 300));
+        setVisible(true);
+    }
+
+    private double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+        final int R = 6371;
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = R * c;
+        DecimalFormat df = new DecimalFormat("#.####");
+        distance = Double.valueOf(df.format(distance));
+        return distance;
+    }
+
+    public static void main(String[] args) {
+        new GPSApp();
+    }
+
+}
